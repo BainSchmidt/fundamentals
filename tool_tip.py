b940125dbb668acdc9a3c602adc5aca27037988f
Bain Schmidt <Bainschmidt@gmail.com>
2022-09-26 13:03:26 -0400

    hello world

---
+++ tool_tip.py
@@ -0,0 +1,16 @@
+greeting = "Hello world!"
+print(greeting)
+favorite_fruits = ["banana", "kiwi", "lychee", "blueberry"]
+num = 77
+parting = "Goodbye!"
+print(parting)
+
+# TASKS:
+
+# Press "RUN"
+# Pause the playback
+# Page back to the beginning
+# Step forward one line at a time
+# Click the Variables and Terminal tabs to see how they work
+# Go back to the "Code" window, and add your own code
+# Hit "reset"
